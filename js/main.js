9348ee2308ba08a0c477a9d3e4cfce948c6ddbc1
Alina Onishchenko <30931781+SiJunma@users.noreply.github.com>
2021-06-04 18:49:33 +0300

    Add mobile submenu first level

--- js/main.js
+++ js/main.js
@@ -50,7 +50,28 @@ $( document ).ready(function() {
     $('#navUser').on('click', (evt) => {
         evt.preventDefault();
         $('#userPopup').fadeToggle(300);
+
+        $(document).on('click', (evt) => {
+            let container = $('#userPopup');
+            let container2 = $('#navUser');
+            if(container.has(evt.target).length === 0 & container2.has(evt.target).length === 0) {
+                $('#userPopup').fadeOut(300);
+            }
+        });
     });
+
+    $('.menu__burger-btn').on('click', (evt) => {
+        evt.preventDefault();
+        $('#navSub').addClass('visible');
+        $('.nav-sub__menu .nav-main__menu').css('display', 'flex');
+    });
+
+    $('.nav-sub__mobile-nav .btn-close').on('click', (evt) => {
+        evt.preventDefault();
+        $('#navSub').removeClass('visible');
+        $('.nav-sub__item').removeClass('visible');
+        $('.menu-dropdown').removeClass('current');
+    })
 
     function searchClose() {
         $('#navSearch').removeClass('search-open');
@@ -70,13 +91,13 @@ $( document ).ready(function() {
         setTimeout(() => {$('#whenChange').fadeIn(400);}, 500)
     }
 
-
     function subMenuOpenDropdown(id) {
         $('.nav-sub__item').removeClass('visible');
         $(`${id}`).addClass('visible');
         $('.nav-sub__menu .menu-dropdown').removeClass('current');
         $(`.nav-sub__menu .menu-dropdown[href="${id}"]`).addClass('current');
     };
+
 
 
 

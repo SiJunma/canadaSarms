f054a5626250315ec3bd514c9040f95b7a75ba93
Alina Onishchenko <30931781+SiJunma@users.noreply.github.com>
2021-07-09 16:39:20 +0300

    article page + adaptive

---
+++ article.php
@@ -0,0 +1,322 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include('head.php'); ?>
+</head>
+<body>
+
+    <?php include('nav.php'); ?>
+
+    <section class="blog-back">
+        <div class="container">
+            <a href="blog.php" class="blog-back__link">Back</a>
+        </div>
+    </section>
+    
+    <main>
+
+        <section class="blog-main-prospekt">
+            <div class="container">
+                <div class="blog-main-prospekt__wrapper">
+                    <div class="blog-main-prospekt__big blog-article">
+
+                        <h3>MK 677: THE NON-SUPPRESSIVE ANABOLIC AGENT</h3>
+
+                        <div class="blog-cart">
+                            <div class="blog-cart__img">
+                                <img src="img/blog-cart1.jpg" alt="MK 677: THE NON-SUPPRESSIVE ANABOLIC AGENT">
+                                <div class="blog-cart__date">february 24, 2021</div>
+                            </div>
+                        
+                            <div class="blog-cart__content">
+                                <h4>MK 677: THE NON-SUPPRESSIVE ANABOLIC AGENT</h4>
+                                <div class="blog-cart__text">
+                                    <p>
+                                        MK 677 or Ibutamoren, while it is often considered in the category of SARMs , it technically isn’t one. It is a potent, orally-active, long-acting, selective growth hormone secretagogue and a non-peptide agonist of the ghrelin receptor.
+                                    </p>
+
+                                    <p>
+                                        In simple words, it mimics growth hormone (GH) and increases the secretion of insulin-like growth factor 1 (IGF-1). The focal importance of growth hormone is, it stimulates growth, cell regeneration, and cell growth in humans and other animals. On the other hand, Ibutamoren does all these things while possibly unaffecting cortisol levels.
+                                    </p>
+
+                                    <p>
+                                        MK 677 is one of the widely known drugs (Not a SARM!) that support increasing lean body mass without changing the total fat mass or visceral fat. Likewise, it is considered to augment both muscle mass and bone mineral density. 
+                                    </p>
+
+                                    <p>
+                                        Due to these main factors, MK 677 or Ibutamoren is a novel name in the bodybuilding community. So, in this article, we venture into all the science-backed potential benefits of MK 677.
+                                    </p>
+                                </div>
+                            </div>
+
+                            <h4>1. May Help Gain Fat-Free Mass</h4>
+
+                            <div class="blog-cart__content">
+                                <p>
+                                    Obesity is often connected with weakened GH secretion, unbalanced body composition, and increased cardiovascular mortality.
+                                </p>
+
+                                <p>
+                                    With that being said, the National Library of Medicine shows the results of a study that had been conducted on healthy obese males. The purpose of this study was to observe the effects on GH secretion and body composition by oral treatment with GH secretagogue MK 677.
+                                </p>
+
+                                <p>
+                                    24 obese males who were aged 18-50 years with body mass indexes greater than 30 kg/m2, and waist/hip ratios greater than 0.95 participated in the study. Then, they were treated with 25 mg of MK 677 for 8 weeks daily. This MK 677 treatment showed about a 40% increase in Serum insulin-like growth factor I (IGF-I). Moreover, the Serum IGF-binding protein-3 was also significantly increased. Likewise, a notable increase in fat-free mass was shown without changing the total and visceral fat. In addition to that, the basal metabolic rate was also increased.
+                                </p>
+
+                                <p>
+                                    Moreover, as stated in ncbi.nlm.nih.gov, a study conducted by administering a growth-hormone secretagogue and a ghrelin mimetic Ibutamoren to healthy elderly individuals showed a great deal of growth hormone augmentation. Not to mention, it also displayed an increase of IGF-I in healthy young adults. In a nutshell, this treatment was well-tolerated by the subjects, and their fat-free mass was increased.
+                                </p>
+
+                                <p>
+                                    It is crucial to understand that fat-free mass should be maintained during weight loss. Further studies suggest that fat-free mass plays a vital role in metabolic rate regulation, preserving skeletal integrity and maintaining functional capacity.
+                                </p>
+
+                                <p>
+                                    So, it is evident that Ibutamoren may be quite effective when it comes to losing fat.
+                                </p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="blog-main-prospekt__aside">
+                        <div class="blog-cart_mailing blog-article_mailing">
+                            <h4>Join Our Mailing List</h4>
+    
+                            <form>
+                               <input type="email" placeholder="email address..."> 
+                               <button class="btn" type="submit">subscribe</button>
+                            </form>
+                        </div>
+
+                        <h3>RELated posts</h3>
+    
+                        <?php 
+                            $title = 'RAD-140: THE KING OF SARMS';
+                            $img = 'blog-cart2.jpg';
+                            $date = 'february 24, 2021';
+                            $text = "If you are keen on gathering more info on the fitness and health industry, you might have stumbled upon the name RAD-140 which is also known as Testolone...";
+    
+                            include('blog-cart.php');
+                        ?>
+
+                        <?php 
+                            $title = 'What are SARMs?';
+                            $img = 'blog-cart3.jpg';
+                            $date = 'february 24, 2021';
+                            $text = "If you are keen on gathering more info on the fitness and health industry, you might have stumbled upon the name RAD-140 which is also known as Testolone...";
+    
+                            include('blog-cart.php');
+                        ?>
+
+                        <?php 
+                            $title = '“Sarms Where to Buy” – 3 Things to Know';
+                            $img = 'blog-cart4.jpg';
+                            $date = 'february 24, 2021';
+                            $text = "If you are keen on gathering more info on the fitness and health industry, you might have stumbled upon the name... ";
+    
+                            include('blog-cart.php');
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="interested">
+            <div class="container">
+                <div class="interested__wrapper">
+                    <div class="interested__line">
+                        <h3>you may be interested</h3>
+                        <a href="shop.php" class="blog-cart__link">view more</a>
+                    </div>
+
+                    <div class="interested__slider">
+                        <div>
+                            <div class="interested__item">
+                                <div class="interested__img">
+                                    <img src="img/product-sale2.jpg" alt="Sale">
+                                </div>
+
+                                <span class="interested__title">
+                                    fitness stack
+                                </span>
+
+                                <b class="interested__price">$149 – $429</b>
+
+                                <a href="#" class="btn btn_blue">add to cart</a>
+                            </div>
+                        </div>
+
+                        <div>
+                            <div class="interested__item">
+                                <div class="interested__img">
+                                    <img src="img/product-sale2.jpg" alt="Sale">
+                                </div>
+
+                                <span class="interested__title">
+                                    Get Shredded – Athlete Stack
+                                </span>
+
+                                <b class="interested__price">$149 – $429</b>
+
+                                <a href="#" class="btn btn_blue">add to cart</a>
+                            </div>
+                        </div>
+
+                        <div>
+                            <div class="interested__item">
+                                <div class="interested__img">
+                                    <img src="img/product-sale3.jpg" alt="Sale">
+                                </div>
+
+                                <span class="interested__title">
+                                    Total Transformation Stack
+                                </span>
+
+                                <b class="interested__price">$249 – $729</b>
+
+                                <a href="#" class="btn btn_blue">add to cart</a>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+        <section class="blog-main-prospekt">
+            <div class="container">
+                <div class="blog-main-prospekt__wrapper">
+                    <div class="blog-main-prospekt__big blog-article">
+
+                        <div class="blog-cart">
+
+                            <h4>2. MK 677 May Significantly Improve Sleep Quality</h4>
+
+                            <div class="blog-cart__content">
+                                <p>
+                                    Regardless of what kind of diet we follow or workout regimen we adapt ourselves to, a good night slumber is the most crucial element that supports us to regenerate our mind and body after a tiresome day. This is why lack of sleep kills our gains.
+                                </p>
+
+                                <p>
+                                    Speaking of which, a study conducted on sleep quality in healthy young and older adults observed the prolonged treatments with MK 677 towards sleep quality. 8 young adults from 18 to 30 years had participated in this study and each one of them went through three 7-day treatment periods with 5 and 25 mg doses of Ibutamoren. Then, 6 older subjects of ages from 65 to 71 were treated with 2 and 25 mg doses of MK 677.
+                                </p>
+
+                                <p>
+                                    In young subjects who were treated with higher doses of MK 677 seemed to have over a 20% increase in REM sleep whereas older adults had shown a 50% increase in REM sleep. 
+                                </p>
+
+                                <p>
+                                    Thus, the overall study shows that MK 677 may be a potential treatment for enhancing sleep quality. However, this study clearly emphasizes the dosage of MK 677 and its relevance in accommodating better results for sleep quality.
+                                </p>
+
+                                <p>
+                                    Therefore, if you were to use Ibutamoren, it is noteworthy that you should take its recommended dosage (under the supervision of your doctor only).
+                                </p>
+                            </div>
+
+                            <h4>3. Potentially Useful in Treating Catabolic Conditions (Muscle Preservation)</h4>
+
+                            <div class="blog-cart__content">
+                                <p>
+                                    A catabolic condition refers to a state that when your body loses fat and skeletal muscle mass at a rapid pace. However, not in a good way. It is primarily because of a disease that is lingering in the background such as an injury or infection. Sometimes this could be diabetic ketoacidosis, multisystem organ failure, AIDS, advanced cancer, etc.
+                                </p>
+
+                                <p>
+                                    An article published in Ncbi.nlm.nih.gov discusses a study that has been carried out to determine whether Ibutamoren can reverse diet-induced protein catabolism. This specific study included 8 healthy subjects that are aged between 24 and 39. They were first calorically restricted for two 14-day periods. These subjects were given either oral MK 677 25 mg or placebo once a day for the last 7 days of each diet period.
+                                </p>
+
+                                <p>
+                                    The study showed an improved nitrogen balance due to MK 677 administration. Likewise, MK 677 (25 mg) was well tolerated by the subjects during the test period. Therefore, this study concludes that MK 677 may in fact reverse diet-induced nitrogen wasting. This means, if these positive yet short-term anabolic effects are maintained in catabolic patients who are suffering from an acute or chronic disease state, Ibutamoren can be a powerful treatment for treating catabolic conditions.
+                                </p>
+
+                            </div>
+
+                            <h4>verdict</h4>
+
+                            <div class="blog-cart__content">
+                                <p>
+                                    There is a lot of questions out there regarding MK 677 and many people are wondering whether it can be used to improve their overall health. Despite what various products on the market imply, as intelligent consumers, the key is to understand what is contained in SARMs and similar products and rely on science-backed data (while firstly consulting a family doctor).
+                                </p>
+
+                                <p>
+                                    So, these studies mentioned above prove that Ibutamoren may be a reliable and potential treatment for gaining muscle mass, improving sleep quality, inhibiting catabolic conditions, and prolonging the equilibrium of the body. As discussed before, Ibutamoren  may play a vital role in creating a healthy initiative for obese people as well.
+                                </p>
+
+                                <p>
+                                    However, if you look closely, all the studies suggest a particular dose to be administered in order to get the most out of it. If used properly, MK 677 can be an ideal source to jump-start your metabolic rate and promote healthy living while gradually losing weight (if your doctor recommends its use).
+                                </p>
+
+                            </div>
+
+                            <h4>References</h4>
+
+                            <div class="blog-cart__content">
+                                <a href="https://en.wikipedia.org/wiki/Ibutamoren">
+                                    wiki/Ibutamoren
+                                </a> <br>
+                                <a href="https://en.wikipedia.org/wiki/Growth_hormone">
+                                    wiki/Growth_hormone
+                                </a> <br>
+                                <a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3898933/">
+                                    ncbi.nlm.nih.gov
+                                </a> <br>
+                                <a href="https://pubmed.ncbi.nlm.nih.gov/9467542/">
+                                    pubmed.ncbi.nlm.nih.gov
+                                </a> <br>
+                                <a href="https://pubmed.ncbi.nlm.nih.gov/8923645/">
+                                    pubmed.ncbi.nlm.nih.gov
+                                </a> <br>
+                                <a href="https://pubmed.ncbi.nlm.nih.gov/9349662/">
+                                    pubmed.ncbi.nlm.nih.gov
+                                </a> <br>
+                                <a href="https://pubmed.ncbi.nlm.nih.gov/9467534/">
+                                    pubmed.ncbi.nlm.nih.gov
+                                </a> <br>
+                                <a href="https://medical-dictionary.thefreedictionary.com/catabolic+illness">
+                                    medical-dictionary
+                                </a> <br>
+                            </div>
+
+                            <div class="blog-cart__content_noborder blog-cart__content_reply">
+                                <h4>LEAVE A REPLY</h4>
+                                <small>Your email address will not be published. Required fields are marked *</small>
+                            </div>
+
+                            <div class="blog-cart__content">
+                                <form class="reply__wrapper">
+                                    <textarea name="" id="" cols="30" rows="10" placeholder="Comment"></textarea>
+
+                                    <div class="reply__inputs">
+                                        <input type="text" name="name" placeholder="Name*">
+                                        <input type="text" name="email" placeholder="Email*">
+                                        <input type="text" name="website" placeholder="Website">
+
+                                        <label for="saveData" class="reply__checkbox">
+                                            <input type="checkbox" name="save" id="saveData">
+                                            <span>Save my name, email, and website in this browser for the next time I comment.</span>
+                                        </label>
+
+                                        <button class="btn">post comment</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+    </main>
+
+    <?php include('footer.php'); ?>
+    
+    <?php include('scripts.php'); ?>
+
+
+</body>
+</html>

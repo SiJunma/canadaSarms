6e0cf28f64d0df67b97272a3fb7a11eddf45fb6d
Alina <sijunma116@gmail.com>
2021-07-26 14:00:24 +0300

    about page full adaptive

---
+++ about.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include('head.php'); ?>
+</head>
+<body>
+
+    <?php include('nav.php'); ?>
+
+    <header class="header-regular">
+        <div class="container">
+            <h3>About</h3>
+
+            <div class="breadcrumbs">
+                <ul class="breadcrumbs__list">
+                    <li>
+                        <a href="index.php">Home</a>
+                    </li>
+                    <li> 
+                        <a href="#" class="current">About</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <section class="about-desc">
+            <div class="container">
+                <div class="about-desc__wrapper">
+                    <div class="about-desc__text-content">
+                        <p>
+                            Canada Sarms is a leading provider for research SARMs in Canada. Our mission is to surpass your expectations with our superior quality control. All our products are produced, packaged and shipped from Quebec, Canada. All our SARMs are pharmaceutical grade, lab tested and contain no alcohol, fillers or any illegal substance.
+                        </p>
+                        <p>
+                            Canada Sarms discreet shipping is both fast AND secure. All our products are intentionally crafted with excellence and are made with 100% Kosher & Halal ingredients. We offer the most potent formula on the market. Stop overpaying for other brands that often sell questionable, untested SARMs. We work hard to offer you the best possible prices, without compromising the high quality you love. With innovative technologies such as research SARMs, the 21st century is the ideal era to pursue your research goals. If you already tried our products, you know you can count on Canada Sarms to achieve your goals with confidence. 
+                        </p>
+                    </div>
+                    <div class="about-desc__media">
+                        <img src="img/video-placeholder.png" alt="placeholder">
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="about-triggers">
+            <div class="container">
+                <div class="about-triggers__wrapper">
+                    <div class="about-triggers__item">
+                        <div class="about-triggers__img">
+                            <img src="img/logistics.svg" alt="FREE GLOBAL SHIPPING">
+                        </div>
+                        <div class="about-triggers__content">
+                            <b class="about-triggers__title">
+                                FREE GLOBAL SHIPPING
+                            </b>
+                            <p class="about-triggers__text">
+                                On All Orders Over $299 <br>
+                                Worldwide – Fast – Secure
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="about-triggers__item">
+                        <div class="about-triggers__img">
+                            <img src="img/guarantee.svg" alt="CANADA SARMS GUARANTEE">
+                        </div>
+                        <div class="about-triggers__content">
+                            <b class="about-triggers__title">
+                                CANADA SARMS GUARANTEE
+                            </b>
+                            <p class="about-triggers__text">
+                                Pharmaceutical Grade <br>
+                                Made & Tested in Canada
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="about-triggers__item">
+                        <div class="about-triggers__img">
+                            <img src="img/productivity.svg" alt="MAX POTENCY FORMULA">
+                        </div>
+                        <div class="about-triggers__content">
+                            <b class="about-triggers__title">
+                                MAX POTENCY FORMULA
+                            </b>
+                            <p class="about-triggers__text">
+                                Enhanced Bioavailability <br>
+                                Get the Most out of your SARMs
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+
+    <?php include('footer.php'); ?>
+
+    <?php include('scripts.php'); ?>
+
+</body>
+</html>

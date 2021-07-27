b60c8b9086e85e9d8d63b89e76b197d2fde7f1f9
Alina <sijunma116@gmail.com>
2021-07-27 16:05:58 +0300

    guide page full adaptive

--- guide.php
+++ guide.php
@@ -25,7 +25,162 @@
     </header>
 
     <main>
+        <section class="guide-instruction">
+            <div class="container">
+                <div class="guide-instruction__wrapper">
+                    <span class="guide-instruction__note">
+                        Shake well before using. Keep in a cool, dry place.
+                    </span>
+    
+                    <ul class="guide-instruction__list">
+                        <li>
+                            <img src="img/sun.svg" alt="Our Bottles">
+                            <p>
+                                Our Bottles provide protection from light. If you ever decide to put the liquid in a transparent bottle, make sure to keep away from light
+                            </p>
+                        </li>
+                        <li>
+                            <img src="img/thermometer.svg" alt="Store at room">
+                            <p>
+                                Store at room temperature. Do not expose to heat or cold
+                            </p>
+                        </li>
+                        <li>
+                            <img src="img/dropper.svg" alt="Store at room">
+                            <p>
+                                This is simply an informative general guide on dosage
+                            </p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
 
+        <section class="tabs-section tabs_guide">
+            <div class="container">
+                <div class="tabs-section__wrapper">
+                    <div class="cd-tabs">
+                        <div class="cd-tabs__nav">
+                            <div class="cd-tabs-navigation">
+                                <div>
+                                    <a data-content="For MEN" class="selected" href="#0">For MEN</a>
+                                </div>
+                                <div>
+                                    <a data-content="For WOMEN" class="" href="#1">For WOMEN</a>
+                                </div>
+                            </div>
+                        </div>
+                        <ul class="cd-tabs-content">
+                            <li data-content="For MEN" class="selected">
+                                <ul class="guide-list">
+                                    <li>
+                                        <span class="guide-list__title">LGD-4033</span>
+                                        <p class="guide-list__value">
+                                            0.5-1ml/day. Recommended length: 30-60 days.
+                                        </p>
+                                    </li>
+                                    <li>
+                                        <span class="guide-list__title">RAD-140:</span>
+                                        <p class="guide-list__value">
+                                            1-3 ml/day. Recommended length: 30-90 days.
+                                        </p>
+                                    </li>
+                                    <li>
+                                        <span class="guide-list__title">OSTARINE:</span>
+                                        <p class="guide-list__value">
+                                            1-2 ml/day. Recommended length: 60-90 days.
+                                        </p>
+                                    </li>
+                                    <li>
+                                        <span class="guide-list__title">MK-677:</span>
+                                        <p class="guide-list__value">
+                                            1ml/day at bedtime. Recommended length: 90-120 days.
+                                        </p>
+                                    </li>
+                                    <li>
+                                        <span class="guide-list__title">Nolva (Liquid):</span>
+                                        <p class="guide-list__value">
+                                            1ml/day in the morning for 2 weeks, then 0.5ml/day for 2 weeks.
+                                        </p>
+                                    </li>
+                                    <li>
+                                        <span class="guide-list__title">Nolva (Pills):</span>
+                                        <p class="guide-list__value">
+                                            2 pills/day in the morning for 2 weeks, then 1 pill/day for 2 weeks.
+                                        </p>
+                                    </li>
+                                    <li>
+                                        <span class="guide-list__title">GW-501516:</span>
+                                        <p class="guide-list__value">
+                                            1ml/day. Recommended length: 60-90 days.
+                                        </p>
+                                    </li>
+                                    <li>
+                                        <span class="guide-list__title">SR-9009:</span>
+                                        <p class="guide-list__value">
+                                            0.5ml 3x per day (total: 1.5ml/day). Recommended length: 60-120 days.
+                                        </p>
+                                    </li>
+                                </ul>
+                            </li>
+
+                            <li data-content="For WOMEN" class="">
+                                <ul class="guide-list">
+                                    <li>
+                                        <span class="guide-list__title">LGD-4033</span>
+                                        <p class="guide-list__value">
+                                            0.5-1ml/day. Recommended length: 30-90 days.
+                                        </p>
+                                    </li>
+                                    <li>
+                                        <span class="guide-list__title">RAD-140:</span>
+                                        <p class="guide-list__value">
+                                            0.5-1ml/day. Recommended length: 30-90 days.
+                                        </p>
+                                    </li>
+                                    <li>
+                                        <span class="guide-list__title">OSTARINE:</span>
+                                        <p class="guide-list__value">
+                                            1ml/day. Recommended length: 30-90 days.
+                                        </p>
+                                    </li>
+                                    <li>
+                                        <span class="guide-list__title">MK-677:</span>
+                                        <p class="guide-list__value">
+                                            1ml/day at bedtime. Recommended length: 60-120 days.
+                                        </p>
+                                    </li>
+                                    <li>
+                                        <span class="guide-list__title">Nolva (Liquid):</span>
+                                        <p class="guide-list__value">
+                                            1ml/day in the morning for 2 weeks, then 0.5ml/day for 2 weeks.
+                                        </p>
+                                    </li>
+                                    <li>
+                                        <span class="guide-list__title">Nolva (Pills):</span>
+                                        <p class="guide-list__value">
+                                            2 pills/day in the morning for 2 weeks, then 1 pill/day for 2 weeks.
+                                        </p>
+                                    </li>
+                                    <li>
+                                        <span class="guide-list__title">GW-501516:</span>
+                                        <p class="guide-list__value">
+                                            1ml/day. Recommended length: 60-90 days.
+                                        </p>
+                                    </li>
+                                    <li>
+                                        <span class="guide-list__title">SR-9009:</span>
+                                        <p class="guide-list__value">
+                                            1 ml/day divided in 3 daily doses. Recommended length: 60-120 days.
+                                        </p>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
 
     </main>
 
